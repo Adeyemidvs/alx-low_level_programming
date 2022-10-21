c17149a0c98ad9e752cc006f5c268dec8df1beba
Adeyemidvs <adeyemidvs@outlook.com>
2022-10-20 18:31:12 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  _isupper - checks if uppercase
- *  @c: character to check
+ * _isupper - checks if uppercase
+ * @c: character to check
  *
- *  Return: gives 1 or 0
+ * Return: gives 1 or 0
  */
 int _isupper(int c)
 {
